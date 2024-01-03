aa26ec624ce0eb9c74465fc2f54835759cd62e0a
Noor Mohammed Priom <noor105464@gmail.com>
2024-01-03 08:53:45 +0600

    P

--- 2023/UAP Junior Programming Contest 2024 (1-1 to 2-2)/Source Codes/C/H.c
+++ 2023/UAP Junior Programming Contest 2024 (1-1 to 2-2)/Source Codes/C/H.c
@@ -1,8 +1,35 @@
 // H - বাশ বিক্রি 🎍
 
 #include <stdio.h>
+#include<stdlib.h>
+
+//comapre() function for qsort
+int compare(const void *x, const void *y){
+    int a = *(int *)x;
+    int b = *(int *)y;
+    return b - a;
+}
 
 int main() {
+    int i, j, n, trees[100134], days = -100000;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++){
+        scanf("%d", &trees[i]);
+    }
 
+    //sort
+    qsort(trees, n, sizeof(int), compare);
+
+    //finding days
+    j = 1;
+    for(i = 0; i < n; i++){
+        if(trees[i] + j > days){
+            days = trees[i] + j;
+        }
+        j++;
+    }
+    days++;
+
+    printf("%d\n", days);
     return 0;
 }
--- 2023/UAP Junior Programming Contest 2024 (1-1 to 2-2)/Source Codes/C/I.c
+++ 2023/UAP Junior Programming Contest 2024 (1-1 to 2-2)/Source Codes/C/I.c
@@ -1,8 +1,29 @@
 // I - যোগফল জিজ্ঞাসা 🤔
 
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-
+long long int prefix_sum[230000];
+int main()
+{
+    int i, n, q, x, y, array[230000];
+    long long int sum;
+    scanf("%d %d", &n, &q);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    //making prefix sum
+    prefix_sum[0] = array[0];
+    for(i = 1; i < n; i++)
+    {
+        prefix_sum[i] =prefix_sum[i-1] + array[i];
+    }
+    //processing queries
+    while(q--)
+    {
+        scanf("%d %d", &x, &y);
+        sum = prefix_sum[y - 1] - prefix_sum[x - 1 - 1];
+        printf("%lld\n", sum);
+    }
     return 0;
 }
