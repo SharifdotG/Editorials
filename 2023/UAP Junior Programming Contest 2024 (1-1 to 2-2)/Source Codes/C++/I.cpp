9d7cc6cdff5e9dc1d14e48a19a7ff54dde0124ee
Noor Mohammed Priom <noor105464@gmail.com>
2024-01-04 09:30:42 +0600

    P

--- 2023/UAP Junior Programming Contest 2024 (1-1 to 2-2)/Source Codes/C++/I.cpp
+++ 2023/UAP Junior Programming Contest 2024 (1-1 to 2-2)/Source Codes/C++/I.cpp
@@ -4,7 +4,24 @@
 
 using namespace std;
 
-int main() {
+long long int prefix_sum[230000];
+int main(){
+    int i, n, q, x, y, array[230000];
+    long long int sum;
+    cin>>n>>q;
+    for(i = 0; i < n; i++){
+        cin>>array[i];
+    }
 
+    prefix_sum[0] = array[0];
+    for(i = 1; i < n; i++){
+        prefix_sum[i] = prefix_sum[i - 1] + array[i];
+    }
+
+    while(q--){
+        scanf("%d %d", &x, &y);
+        sum = prefix_sum[y - 1] - prefix_sum[x - 1 - 1];
+        cout<<sum<<endl;
+    }
     return 0;
 }
--- 2023/UAP Junior Programming Contest 2024 (1-1 to 2-2)/Source Codes/C/I.c
+++ 2023/UAP Junior Programming Contest 2024 (1-1 to 2-2)/Source Codes/C/I.c
@@ -3,24 +3,20 @@
 #include<stdio.h>
 
 long long int prefix_sum[230000];
-int main()
-{
+int main(){
     int i, n, q, x, y, array[230000];
     long long int sum;
     scanf("%d %d", &n, &q);
-    for(i = 0; i < n; i++)
-    {
+    for(i = 0; i < n; i++){
         scanf("%d", &array[i]);
     }
-    //making prefix sum
+
     prefix_sum[0] = array[0];
-    for(i = 1; i < n; i++)
-    {
-        prefix_sum[i] =prefix_sum[i-1] + array[i];
+    for(i = 1; i < n; i++){
+        prefix_sum[i] = prefix_sum[i - 1] + array[i];
     }
-    //processing queries
-    while(q--)
-    {
+
+    while(q--){
         scanf("%d %d", &x, &y);
         sum = prefix_sum[y - 1] - prefix_sum[x - 1 - 1];
         printf("%lld\n", sum);
